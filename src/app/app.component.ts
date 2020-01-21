4b66722dec852fce86790275a8b67b757d73d0b2
Warwick De Kock <pair+warwick.dekock@CVSHealth.com>
2020-01-21 12:29:00 -0600

    adding course list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,14 @@ export class AppComponent {
   title = 'CVS Bootcamp!';
   coursesList = false;
   header = 'Choose Option';
+  courses = ['Course 001', 'Course 002', 'Course 003', 'Course 004', 'Course 005'];
+  list = [{courseId: 'CC001', courseName: 'Angular'}, {courseId: 'CC002', courseName: 'Java'}];
+
+
   oncourses() {
     this.coursesList = true;
     this.header = 'Course List';
+    this.title = 'my courses';
   }
 
 }
