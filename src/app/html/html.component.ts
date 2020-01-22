b7f75d729d7e717a2657d0cfdc7940eec087ad6b
Amarpreet Komal <amar.komal@gmail.com>
2020-01-22 17:46:55 -0600

    add html list

--- src/app/html/html.component.ts
+++ src/app/html/html.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './html.component.html',
   styleUrls: ['./html.component.scss']
 })
-export class HtmlComponent implements OnInit {
+export class HtmlComponent {
 
-  constructor() { }
+  htmlPlan = [
+    {
+      status: ['Not Started', 'In Progress', 'Done'],
+      htmlSkill: 'Angular',
+      link: '/angular'
+    },
+    {
+      status: 'Not Started',
+      htmlSkill: 'html',
+      link: '/html',
+    },
+    {
+      status: 'Not Started',
+      htmlSkill: 'HTML',
+      link: '/html',
+    },
+    {
+      status: 'Not Started',
+      htmlSkill: 'CSS',
+      link: '/css',
+    },
+    {
+      status: 'Not Started',
+      htmlSkill: 'JavaScript',
+      link: '/javascript',
+    },
 
-  ngOnInit() {
-  }
+  ];
 
 }
