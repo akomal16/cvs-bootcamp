c2233c78d973da1e2ed9c00048cbcfe97f530d9c
Amarpreet Komal <amar.komal@gmail.com>
2020-01-27 07:55:35 -0600

    create htmlData service

--- src/app/html/html.component.ts
+++ src/app/html/html.component.ts
@@ -1,41 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {CourseService} from "../services/course-service";
 
 @Component({
   selector: 'app-html',
   templateUrl: './html.component.html',
   styleUrls: ['./html.component.scss']
 })
-export class HtmlComponent {
+export class HtmlComponent implements OnInit, OnDestroy {
+  constructor(private courseService: CourseService) {
+  }
+
   title = 'HTML Topics';
 
   htmlPlan = [
-    {
-      htmlSkill: 'HTML 5',
-      status: 'Done',
-    },
-    {
-      htmlSkill: 'HTML Elements',
-      status: 'Done',
-    },
-    {
-      htmlSkill: 'DOM',
-      status: 'Not Started',
-    },
-    {
-      htmlSkill: 'Form',
-      status: 'Done',
-    },
-    {
-      htmlSkill: 'Styles',
-      status: 'Done',
-    },
-    {
-      htmlSkill: 'Events',
-      status: 'Not Started',
-    },
-    {
-      htmlSkill: 'SVG',
-      status: 'In Progress',
-    },
+    // {
+    //   htmlSkill: 'HTML 5',
+    //   status: 'Done',
+    // },
+    // {
+    //   htmlSkill: 'HTML Elements',
+    //   status: 'Done',
+    // },
+    // {
+    //   htmlSkill: 'DOM',
+    //   status: 'Not Started',
+    // },
+    // {
+    //   htmlSkill: 'Form',
+    //   status: 'Done',
+    // },
+    // {
+    //   htmlSkill: 'Styles',
+    //   status: 'Done',
+    // },
+    // {
+    //   htmlSkill: 'Events',
+    //   status: 'Not Started',
+    // },
+    // {
+    //   htmlSkill: 'SVG',
+    //   status: 'In Progress',
+    // },
   ];
+
+  ngOnInit(): void {
+    console.log('This is on html component initialization');
+    // This is calling getJavaData() from the CourseService
+    this.htmlPlan = this.courseService.getHtmlData();
+    // this.courseService.getHtmlData();
+    alert('Hello from HTML component');
+    console.log(this.htmlPlan, 'This is our htmlPlan data');
+  }
+  ngOnDestroy() {
+    console.log('This is on HTML component removal');
+  }
+
 }
--- src/app/services/course-service.ts
+++ src/app/services/course-service.ts
@@ -43,32 +43,38 @@ export class CourseService {
           status: 'Not Started',
         },
       ];
+     }
 
-    // javaPlan = [
-    //   {
-    //     javaSkill: 'Java Syntax',
-    //     status: 'Done',
-    //   },
-    //   {
-    //     javaSkill: 'OOP',
-    //     status: 'In Progress',
-    //   },
-    //   {
-    //     javaSkill: 'Exception Handling',
-    //     status: 'Not Started',
-    //   },
-    //   {
-    //     javaSkill: 'Multi Threading',
-    //     status: 'In Progress',
-    //   },
-    //   {
-    //     javaSkill: 'Collections',
-    //     status: 'In Progress',
-    //   },
-    //   {
-    //     javaSkill: 'Java IO',
-    //     status: 'Not Started',
-    //   },
-    // ];
-  }
+    getHtmlData() {
+          return [
+            {
+              htmlSkill: 'HTML 5',
+              status: 'Done',
+            },
+            {
+              htmlSkill: 'HTML Elements',
+              status: 'Done',
+            },
+            {
+              htmlSkill: 'DOM',
+              status: 'Not Started',
+            },
+            {
+              htmlSkill: 'Form',
+              status: 'Done',
+            },
+            {
+              htmlSkill: 'Styles',
+              status: 'Done',
+            },
+            {
+              htmlSkill: 'Events',
+              status: 'Not Started',
+            },
+            {
+              htmlSkill: 'SVG',
+              status: 'In Progress',
+            },
+          ];
+    }
 }
